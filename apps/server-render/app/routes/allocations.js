9b8974911c05c1b9ce2d90def2da909d3cc95966
cx-lucas-santos-ferreira <215082630+cx-lucas-santos-ferreira@users.noreply.github.com>
2025-09-03 18:04:38 -0300

    test

--- apps/server-render/app/routes/allocations.js
+++ apps/server-render/app/routes/allocations.js
@@ -8,7 +8,7 @@ function AllocationsHandler(db) {
     this.displayAllocations = (req, res, next) => {
         /*
         // Fix for A4 Insecure DOR -  take user id from session instead of from URL param
-        const { userId } = req.session;
+        const { userId } = req.session; 
         */
         const {
             userId
